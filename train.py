12378914e4305842abd4ce4001cc347d1e66d3bb
May Chan <91612351+maylingchan22@users.noreply.github.com>
2022-10-17 01:08:43 -0400

    Pytorch model and train epoch

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from model import NeuralNetwork
 
 with open('chatbot_tags.json', 'r') as t:
     intents = json.load(t)
@@ -23,7 +24,6 @@ ignore_words = ['?', '!', '.', ',']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
-print(tags)
 
 x_train = []
 y_train = []
@@ -44,11 +44,43 @@ class ChatDataset(Dataset):
         self.x_data = x_train
         self.y_data = y_train
     def __getitem__(self, index):
-        return self.x_data[i], self.y_data[i]
+        return self.x_data[index], self.y_data[index]
     def __len__(self):
         return self.n_samples
 
 batch_size = 8
+hidden_size = len(x_train[1])
+output_size = len(tags)
+input_size = len(x_train[0])
+learning_rate = 0.001
+num_epochs = 1000
+
+
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = NeuralNetwork(input_size, hidden_size, output_size).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device)
+
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch + 1) % 100 == 0:
+        print(f'epoch {epoch+1}/{num_epochs}, loss={loss.item():.4f}')
+
+print(f'final loss, loss={loss.item():.4f}')
+
+
 
