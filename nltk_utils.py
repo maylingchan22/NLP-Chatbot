70c5de1a56594cd24492b1d6d0366ce49dd52e53
May Chan <91612351+maylingchan22@users.noreply.github.com>
2022-10-16 21:15:00 -0400

    Creating train data for json file

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,4 +1,5 @@
 import nltk
+import numpy as np
 nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
@@ -10,8 +11,14 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype =np.float32)
+    for i, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[i] = 1
+    return bag
 
-word = ["Organize", "organizes", "organizing"]
-stemmed_words = [stem(w) for w in word]
-print(stemmed_words)
+sentence = ["hello", "how", "are", "you"]
+words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+bag = bag_of_words(sentence, words)
+print(bag)
