72bd7d768e912cc0c8e52f3ed23b297f6ecac86c
May Chan <91612351+maylingchan22@users.noreply.github.com>
2022-10-16 20:30:11 -0400

    Stemming and Tokenizing

---
+++ nltk_utils.py
@@ -0,0 +1,17 @@
+import nltk
+nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
+
+word = ["Organize", "organizes", "organizing"]
+stemmed_words = [stem(w) for w in word]
+print(stemmed_words)
